4de0b8deb5f99ddfec944b61a020ee06bc4d74bf
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-03-10 12:39:02 +1300

    Set version to 0.11.2.

--- mapclientplugins/scaffoldcreator/__init__.py
+++ mapclientplugins/scaffoldcreator/__init__.py
@@ -3,7 +3,7 @@
 MAP Client Plugin
 """
 
-__version__ = '0.11.1'
+__version__ = '0.11.2'
 __author__ = 'Richard Christie'
 __stepname__ = 'Scaffold Creator'
 __location__ = 'https://github.com/ABI-Software/mapclientplugins.scaffoldcreator'
